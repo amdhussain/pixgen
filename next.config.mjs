898ac3e32da2cce08c7016e5db54c52c51334ddf
Md Hussain Ahmed <amdhussain538@gmail.com>
2026-06-03 12:59:17 +0600

    added a new signup and signin

--- next.config.mjs
+++ next.config.mjs
@@ -15,9 +15,9 @@ const nextConfig = {
       },
     ],
   },
-  experimental: {
-serverComponentsExternalPackages: ['@better-auth/kysely-adapter'],
-},
+
+ serverExternalPackages: ['@better-auth/kysely-adapter'],
+ 
 };
 
 export default nextConfig;
