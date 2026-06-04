4120f7f94df523117a302b083076c1fbadb7ac38
Md Hussain Ahmed <amdhussain538@gmail.com>
2026-06-04 21:53:57 +0600

    added a new signout

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -7,14 +7,21 @@
 
 
 
-// lib/auth-client.js
+
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || "http://localhost:3000",
+  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL  
 });
 
 export const { signIn, signOut, useSession } = authClient;
 
+
+
+
+// export const authClient = createAuthClient({
+//     // ডিফল্ট হিসেবে localhost যোগ করে রাখা নিরাপদ
+//     baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
+// })
 
   
